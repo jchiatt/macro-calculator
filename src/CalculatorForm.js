cb5d1d43961bffe8d801c0b035493fe3c62277eb
J.C. Hiatt <jc@jchiatt.com>
2019-09-15 23:21:19 -0500

    feat: extend calculations to offer different ratio options

--- src/CalculatorForm.js
+++ src/CalculatorForm.js
@@ -1,9 +1,23 @@
 import React from 'react';
-function calculateMacros(weight) {
-  const totalCalories = weight * 11;
-  const proteinInGrams = (Math.round(((totalCalories / 2) / 4) * 10) / 10)
-  const carbsInGrams = (Math.round(((totalCalories / 4) / 4) * 10) / 10)
-  const fatInGrams = (Math.round(((totalCalories / 4) / 9) * 10) / 10)
+function calculateMacros(weight, ratio) {
+  var totalCalories;
+  var proteinInGrams;
+  var carbsInGrams;
+  var fatInGrams;
+
+  if ( ratio === "fatloss" ) {
+    totalCalories = weight * 11;
+    proteinInGrams = Math.round((totalCalories / 2 / 4) * 10) / 10;
+    carbsInGrams = Math.round((totalCalories / 4 / 4) * 10) / 10;
+    fatInGrams = Math.round((totalCalories / 4 / 9) * 10) / 10;
+  }
+
+  if ( ratio === "moderate" ) {
+    totalCalories = weight * 11;
+    proteinInGrams = Math.round(((totalCalories * .4) / 4) * 10) / 10;
+    carbsInGrams = Math.round(((totalCalories * .3) / 4) * 10) / 10;
+    fatInGrams = Math.round(((totalCalories * .3) / 9) * 10) / 10;
+  }
 
   return {
     proteinInGrams,
@@ -17,6 +31,7 @@ export default function CalculatorForm() {
   const [protein, setProtein] = React.useState(0);
   const [carbs, setCarbs] = React.useState(0);
   const [fat, setFat] = React.useState(0);
+  const [ratio, setRatio] = React.useState("fatloss");
 
   function handleChange(e) {
     if ( e.target.value === '' ) {
@@ -27,7 +42,7 @@ export default function CalculatorForm() {
   }
   function handleSubmit(e) {
     e.preventDefault();
-    const { proteinInGrams, carbsInGrams, fatInGrams } = calculateMacros(weight);
+    const { proteinInGrams, carbsInGrams, fatInGrams } = calculateMacros(weight, ratio);
     setProtein(proteinInGrams);
     setCarbs(carbsInGrams);
     setFat(fatInGrams);
@@ -45,6 +60,28 @@ export default function CalculatorForm() {
             value={weight}
             onChange={handleChange}
           />
+        </label>
+        <label className="Form__radio">
+          <input
+            type="radio"
+            name="ratio"
+            value="fatloss"
+            checked={ratio === "fatloss"}
+            className="Form__input"
+            onClick={() => setRatio("fatloss")}
+          />
+          <span className="Form__span">50%, 25%, 25%</span>
+        </label>
+        <label className="Form__radio">
+          <input
+            type="radio"
+            name="ratio"
+            value="moderate"
+            checked={ratio === "moderate"}
+            className="Form__input"
+            onClick={() => setRatio("moderate")}
+          />
+          <span className="Form__span">40%, 30%, 30%</span>
         </label>
         <button type="submit">Calculate</button>
         {protein > 0 && (
